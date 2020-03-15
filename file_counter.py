a07d45d4a184fbcc3ada87aed86cccdda409ac52
Kartik Nagila <60644731+cybermonkey14@users.noreply.github.com>
2020-03-15 13:16:48 +0530

    Add files via upload

---
+++ file_counter.py
@@ -0,0 +1,23 @@
+#Just a simple program to check the number of specific extension files in a dir.
+import os
+import time
+while True:
+   count = 0
+   print(">>> File counter..")
+   print(">>> Press anything else to exit.")
+   enter_file = input(">>>Enter the extension of file you want to search(with .): ")
+   if enter_file.startswith('.'):
+      for (dirs, dirname, files) in os.walk('.'):
+         for filename in files:
+            if filename.endswith(enter_file):
+               count = count + 1
+      p = ("Found {} files with extension {}".format(count,enter_file))
+      print(p,"\n\n")
+      continue
+   elif enter_file == "q" or enter_file == "Q":
+      print("exiting..")
+      time.sleep(1)
+      break
+   else:
+      print("Invalid choice!\n")
+      continue
